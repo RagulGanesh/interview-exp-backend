e9b563da4fd8733357e30c414738e16167b1f29d
Asaduddin aman <83344388+K-Hemananda-Reddy@users.noreply.github.com>
2023-03-18 13:47:29 +0530

    signup working

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const Post = require("./models/Post");
 
 const app = express();
 
+app.use(express.json())
+
 
 // app.use(function(req, res, next) {
 //   res.header('Access-Control-Allow-Origin', 'http://localhost:4000');
